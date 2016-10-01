6c4eefa44438b1601124992e7df94281e010aae7
Enrico Cassinelli <Enrico Cassinelli>
2016-10-01 11:52:54 +0200

    inizio lavori

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head lang="">
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+<title>MAMP PRO</title>
+<style type="text/css">
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: .9em;
+        color: #000000;
+        background-color: #FFFFFF;
+        margin: 0;
+        padding: 10px 20px 20px 20px;
+    }
+
+    samp {
+        font-size: 1.3em;
+    }
+
+    a {
+        color: #000000;
+        background-color: #FFFFFF;
+    }
+
+    sup a {
+        text-decoration: none;
+    }
+
+    hr {
+        margin-left: 90px;
+        height: 1px;
+        color: #000000;
+        background-color: #000000;
+        border: none;
+    }
+
+    #logo {
+        margin-bottom: 10px;
+        margin-left: 28px;
+    }
+
+    .text {
+        width: 80%;
+        margin-left: 90px;
+        line-height: 140%;
+    }
+</style>
+</head>
+
+<body>
+<p>miao</p>
+
+</body>
+</html>
