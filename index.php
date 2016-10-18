78c846e8979146a14c11ef2c0d9df1b899e1dd19
Enrico Cassinelli <Enrico Cassinelli>
2016-10-18 15:38:39 +0200

    andiamo avanti

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php include("includes/header.html");?>
 <div class="inner">
-      <h1>Indice</h1>
-      <h3>Tipografia</h3>
-      <ul>
-        <li>Stili di Base</li>
-        <li>Bottoni</li>
-        <li>Blockquote e testi speciali</li>
-      </ul>
-      <h3>Colori</h3>
-      <ul>
-        <li>Stili di Base</li>
-        <li>Bottoni</li>
-        <li>Blockquote e testi speciali</li>
-      </ul>
+  <div class="content_box">
+    <h1>Indice</h1>
+    <h3>Tipografia</h3>
+    <ul>
+      <li>Stili di Base</li>
+      <li>Bottoni</li>
+      <li>Blockquote e testi speciali</li>
+    </ul>
+    <h3>Colori</h3>
+    <ul>
+      <li>Stili di Base</li>
+      <li>Bottoni</li>
+      <li>Blockquote e testi speciali</li>
+    </ul>
+  </div>
 </div>
 <?php include("includes/footer.html");?>
---
+++ pages/post.php
@@ -0,0 +1,157 @@
+<?php include("/../includes/header.html");?>
+  <div class="lg-section intro" style="background-image: url('http://langhe.net/wp-content/uploads/2015/04/cantina_web.jpg');">
+    <div class="inner">
+      <div class="flex-column__content content_box">
+        <h1 class="title post--title">Gli eventi da non perdere durante la Fiera Del Tartufo Bianco D’Alba 2016</h1>
+        <h3 class="subtitle post--subtitle">Lasciati guidare dalla famiglia Cocito nella scoperta dei vini del Monferrato in particolare della Barbera, emblema di queste colline</h3>
+      </div>
+    </div>
+  </div>
+  <div class="lg-section">
+    <div class="inner folio">
+      <div class="meta__container meta__container--post">
+        <p class="meta meta--author">
+          <span class="meta__label meta__label--author">Autore:</span> Enrico Cassinelli
+        </p>
+      </div>
+      <div class="flex-column flex-column--2-3">
+        <div class="flex-column__content content_box">
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+
+          <p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
+
+          <p>In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi.</p>
+
+          <h2>Nullam dictum felis eu pede mollis</h2>
+
+          <p>Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</p>
+
+          <p>Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus.</p>
+
+          <p>Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.</p>
+
+          <p>Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo.</p>
+
+          <p>Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien.</p>
+
+          <p>Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla.</p>
+
+          <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu.</p>
+
+          <p>Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Praesent adipiscing.</p>
+
+          <p>Phasellus ullamcorper ipsum rutrum nunc. Nunc nonummy metus. Vestibulum volutpat pretium libero. Cras id dui. Aenean ut eros et nisl sagittis vestibulum. Nullam nulla eros, ultricies sit amet, nonummy id, imperdiet feugiat, pede.</p>
+
+          <p>Sed lectus. Donec mollis hendrerit risus. Phasellus nec sem in justo pellentesque facilisis. Etiam imperdiet imperdiet orci. Nunc nec neque. Phasellus leo dolor, tempus non, auctor et, hendrerit quis, nisi.</p>
+
+          <p>Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Maecenas malesuada. Praesent congue erat at massa. Sed cursus turpis vitae tortor. Donec posuere vulputate arcu. Phasellus accumsan cursus velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, nisi quis porttitor congue, elit erat euismod orci, ac</p>
+        </div>
+      </div>
+      <div class="flex-column flex-column--1-3">
+      </div>
+    </div>
+  </div>
+  <div class="lg-section color-wine padding--large">
+    <div class="inner">
+      <div class="flex-column--1-3">
+      </div>
+      <div class="flex-column--2-3">
+        <div class="flex-column__content content_box">
+          <h2 style="text-align:right;">And this is a FW Section</h2>
+          <h3 style="text-align:right;">With some Text on the side to show the typography</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="lg-section">
+    <div class="inner folio">
+      <div class="flex-column flex-column--2-3">
+        <div class="flex-column__content content_box">
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+
+          <p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
+
+          <p>In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi.</p>
+        </div>
+      </div>
+      <div class="flex-column flex-column--1-3">
+        <div class="flex-column__content content_box">
+          <p>Questo è un testo di prova</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="lg-section color-travel padding--large overlay--dark" style="background-image:url(../img/bute.jpg); background-size: cover;">
+    <div class="inner">
+      <div class="flex-column--1-3">
+      </div>
+      <div class="flex-column--2-3">
+        <div class="flex-column__content content_box">
+          <h2 style="text-align:right;">And this is a FW Section</h2>
+          <h3 style="text-align:right;">With some Text on the side to show the typography</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="lg-section padding--large">
+    <div class="inner">
+      <div class="flex-column flex-column--2-3">
+        <div class="flex-column__content content_box">
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+
+          <p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
+
+          <p>In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi.</p>
+
+          <h2>Nullam dictum felis eu pede mollis</h2>
+
+          <p>Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</p>
+
+          <p>Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus.</p>
+        </div>
+      </div>
+      <div class="flex-column flex-column--1-3">
+        <div class="flex-column__content content_box">
+          <p>Questo è un testo di prova</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="lg-section color-travel padding--large overlay--dark" style="background-image:url(../img/tappi.jpg); background-size: cover;">
+    <div class="inner">
+      <div class="flex-column--1-3">
+      </div>
+      <div class="flex-column--2-3">
+        <div class="flex-column__content content_box">
+          <h2 style="text-align:right;">And this is a FW Section</h2>
+          <h3 style="text-align:right;">With some Text on the side to show the typography</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="lg-section">
+    <div class="inner folio">
+      <div class="flex-column flex-column--2-3">
+        <div class="flex-column__content content_box">
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+
+          <p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
+
+          <p>In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi.</p>
+
+          <h2>Nullam dictum felis eu pede mollis</h2>
+
+          <p>Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</p>
+
+          <p>Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus.</p>
+        </div>
+      </div>
+      <div class="flex-column flex-column--1-3">
+        <div class="flex-column__content content_box">
+          <p>Questo è un testo di prova</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<?php include("/../includes/footer.html");?>
