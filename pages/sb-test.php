d590b0138b35331a67bdfd55ab735334ff362f65
Enrico Cassinelli <Enrico Cassinelli>
2016-10-22 12:31:48 +0200

    cazzate

---
+++ pages/sb-test.php
@@ -0,0 +1,49 @@
+<?php include("/../includes/header.html");?>
+  <div class="lg-section intro intro--overlay" style="background-image: url('https://drscdn.500px.org/photo/142708983/m%3D2048/9fda99b9109532a21d873d3d657ffdd8');">
+    <div class="inner has-folio--lg">
+      <div class="inner__row">
+        <div class="flex-column__content content_box">
+          <h1 class="title post--title">Gli eventi da non perdere durante la Fiera Del Tartufo Bianco D’Alba 2016</h1>
+          <h3 class="subtitle post--subtitle">Lasciati guidare dalla famiglia Cocito nella scoperta dei vini del Monferrato in particolare della Barbera, emblema di queste colline</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="lg-section">
+    <div class="inner folio padding--none">
+      <div class="inner__row">
+        <div class="flex-column flex-column--max padding--medium color-sleep washed">
+          <div class="flex-column__content content_box">
+            <h2>H2 bau</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="lg-section color-alternate--light">
+    <div class="inner padding--none">
+      <div class="inner__row">
+        <div class="flex-column flex-column--max">
+          <ul class="sub-menu" style="display: flex; list-style: none; flex-direction: row; justify-content: space-between; font-family: 'open sans'; text-transform: uppercase;">
+            <li style="padding: 1.5em 0.75em;">Descritpion</li>
+            <li style="padding: 1.5em 0.75em;">Rooms</li>
+            <li style="padding: 1.5em 0.75em;">Events</li>
+            <li style="padding: 1.5em 0.75em;" class="color-travel washed">Contacts</li>
+            <li style="padding: 1.5em 0.75em;" class="color-travel">Book Now</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="lg-section">
+    <div class="inner">
+      <div class="inner__row">
+        <div class="flex-column flex-column--max">
+          <div class="flex-column__content content_box">
+            <h2>H2 bau</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<?php include("/../includes/footer.html");?>
