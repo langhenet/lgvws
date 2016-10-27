7404a2e134557e23d5b4b26fa3c4d182d7db7904
Enrico Cassinelli <Enrico Cassinelli>
2016-10-27 17:59:41 +0200

    sb alternativa

---
+++ pages/sb/caneuva2.php
@@ -0,0 +1,329 @@
+<?php
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   $path .= "/includes/header.php";
+   include_once($path);
+?>
+<div class="page-nav color-alternate--light">
+  <ul class="page-nav__list">
+    <li class="page-nav__element"><a href="#descrizione">Descrizione</a></li>
+    <li class="page-nav__element"><a href="#vini">vini</a></li>
+    <li class="page-nav__element"><a href="#visite">Visite in Azienda</a></li>
+    <li class="page-nav__element button"><a class="button" href="#prenota">Prenota un visita</a></li>
+    <li class="page-nav__element"><a href="#contatti">Contatti</a></li>
+    <li class="page-nav__element extra"><a href="#contatti">Territorio</a></li>
+  </ul>
+</div>
+  <div class="lg-section intro intro--overlay" style="background-image: url('http://langhe.net/wp-content/uploads/2013/04/La-produzione.jpg'); background-size: cover; background-position: center center;">
+    <div class="inner has-folio" style="padding-top:7.5em;padding-bottom:7.5em;">
+      <div class="inner__row">
+        <div class="flex-column__content content_box">
+     <h1 class="sb--title" style="color:#fff;">Azienda Agricola Cà Neuva</h1>
+    <!--        <h3 class="subtitle post--subtitle">Una tipica cascina di Langa per una cantina con una storia secolare alle spalle che ancora oggi produce i vini rappresentativi del territorio ricordando i preziosi insegnamenti delle generazioni passate.</h3> -->
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="descrizione" class="lg-section">
+    <div class="inner folio padding--none color-wine washed">
+      <div class="inner__row">
+        <div class="flex-column flex-column--1-4 padding--medium">
+          <div class="flex-column__content content_box">
+            <p>degustazione a partire</p>
+          </div>
+        </div>
+        <div class="flex-column flex-column--1-4 padding--medium">
+          <div class="flex-column__content content_box">
+            <p>orari</p>
+          </div>
+        </div>
+        <div class="flex-column flex-column--1-4 padding--medium">
+          <div class="flex-column__content content_box">
+            <p>contatti</p>
+          </div>
+        </div>
+        <div class="flex-column flex-column--1-4 padding--medium">
+          <div class="flex-column__content content_box">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="leg-section">
+    <div class="inner">
+      <div class="inner__row color-alternate--light">
+        <div class="flex-column flex-column--max padding--medium color-main">
+          <div class="flex-column__content content_box">
+            <h3 style="text-align:center; font-family: PT serif; font-style:italic; font-weight:normal; max-width:770px; margin:0 auto;">Una tipica cascina di Langa per una cantina con una storia secolare alle spalle che ancora oggi produce i vini rappresentativi del territorio ricordando i preziosi insegnamenti delle generazioni passate.</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="lg-section">
+    <div class="inner padding--medium">
+      <div class="inner__row">
+        <div class="flex-column flex-column--2-3">
+          <div class="flex-column__content content_box">
+            <p>La cascina Cà Neuva di <strong>Sergio Abbona</strong> è un’azienda agricola capace di soddisfare le esigenze di tutti coloro che amano il vino.</p>
+            <p>Situata in uno dei luoghi più caratteristici delle Langhe,<strong> Dogliani</strong>, si estende su una superficie di 10 ettari dove sono distribuiti i vari vigneti, la cantina, il locale degustazione, l’ufficio e l’abitazione.</p>
+
+            <p>L’inizio dell’attività risale al 1924, ma già <strong>verso la fine del 1800 la famiglia Abbona si dedicava alla viticoltura </strong>trasmettendo poi a nipoti e pronipoti l’amore e la passione per questo lavoro.</p>
+
+            <p>Il <strong>rinnovamento graduale nel corso degli anni </strong>e l'adozione di sistemi di lavorazione più moderni e tecnologici gli hanno permesso di ottenere vini selezionati, eleganti, armonici senza però dimenticare la tipicità del Dolcetto.</p>
+            <div class="sb__image-gallery ">
+              <div class="sb__image-container">
+                <img class="sb__image" src="http://langhe.net/wp-content/uploads/2016/07/Uve-180x180.jpg"/>
+              </div>
+              <div class="sb__image-container">
+                <img class="sb__image" src="http://langhe.net/wp-content/uploads/2016/07/vigne-180x180.jpg"/>
+              </div>
+              <div class="sb__image-container">
+                <img class="sb__image" src="http://langhe.net/wp-content/uploads/2016/07/vigne1-180x180.jpg"/>
+              </div>
+              <div class="sb__image-container">
+                <img class="sb__image" src="http://langhe.net/wp-content/uploads/2016/07/vigne2-180x180.jpg"/>
+              </div>
+              <div class="sb__image-container">
+                <img class="sb__image" src="http://langhe.net/wp-content/uploads/2016/07/inverno-180x180.jpg"/>
+              </div>
+              <div class="sb__image-container">
+                <img class="sb__image" src="http://langhe.net/wp-content/uploads/2016/07/logo-1-180x180.png"/>
+              </div>
+              <div class="sb__image-container">
+                <img class="sb__image" src="http://langhe.net/wp-content/uploads/2016/07/Uve-180x180.jpg"/>
+              </div>
+              <div class="sb__image-container">
+                <img class="sb__image" src="http://langhe.net/wp-content/uploads/2016/07/vigne-180x180.jpg"/>
+              </div>
+              <div class="sb__image-container">
+                <img class="sb__image" src="http://langhe.net/wp-content/uploads/2016/07/vigne1-180x180.jpg"/>
+              </div>
+              <div class="sb__image-container">
+                <img class="sb__image" src="http://langhe.net/wp-content/uploads/2016/07/vigne2-180x180.jpg"/>
+              </div>
+              <div class="sb__image-container">
+                <img class="sb__image" src="http://langhe.net/wp-content/uploads/2016/07/inverno-180x180.jpg"/>
+              </div>
+              <div class="sb__image-container">
+                <img class="sb__image" src="http://langhe.net/wp-content/uploads/2016/07/logo-1-180x180.png"/>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="flex-column flex-column--1-3 gutter--none">
+          <div class="inner">
+            <div class="inner__row">
+              <div class="flex-column flex-column--max gutter--none padding--none">
+                <div class="flex-column__content gallery-box">
+
+                </div>
+              </div>
+            </div>
+            <div class="inner__row">
+              <div class="flex-column flex-column--max gutter--small padding--medium color-alternate--light">
+                <div class="flex-column__content content-box">
+                  <ul class="sb__services-list">
+                    <li class="sb__service sb__service--selected">
+                      <img class="sb-service__image" src="/../img/lgicons/business/lgb-access.png">
+                      <span class="tooltiptext">Disabili</span>
+                    </li>
+                    <li class="sb__service">
+                      <img  class="sb-service__image" src="/../img/lgicons/business/lgb-aircond.png"><span class="tooltiptext">Aria condizionata</span></li>
+                    <li class="sb__service">
+                      <img  class="sb-service__image" src="/../img/lgicons/business/lgb-dsales.png">
+                      <span class="tooltiptext">Vendita diretta</span></li>
+                    <li class="sb__service">
+                      <img  class="sb-service__image" src="/../img/lgicons/business/lgb-animals.png">
+                      <span class="tooltiptext">Animali</span></li>
+                    <li class="sb__service">
+                    <img  class="sb-service__image" src="/../img/lgicons/business/lgb-meeting.png">
+                    <span class="tooltiptext">Meeting Room</span></li>
+                    <li class="sb__service sb__service--selected">
+                      <img  class="sb-service__image" src="/../img/lgicons/business/lgb-terrace.png">
+                      <span class="tooltiptext">Terrazza</span></li>
+                    <li class="sb__service">
+                      <img  class="sb-service__image" src="/../img/lgicons/business/lgb-work.png">
+                      <span class="tooltiptext">Visite in Aazienda</span></li>
+                    <li class="sb__service sb__service--selected">
+                      <img  class="sb-service__image" src="/../img/lgicons/business/lgb-internet.png">
+                      <span class="tooltiptext">Internet</span></li>
+                    <li class="sb__service sb__service--selected">
+                      <img  class="sb-service__image" src="/../img/lgicons/business/lgb-bike.png">
+                      <span class="tooltiptext">Affitto Bici</span></li>
+                    <li class="sb__service sb__service--selected">
+                      <img  class="sb-service__image" src="/../img/lgicons/business/lgb-kids.png">
+                      <span class="tooltiptext">Giochi per Bambini</span></li>
+                    <li class="sb__service">
+                      <img  class="sb-service__image" src="/../img/lgicons/business/lgb-parking.png">
+                      <span class="tooltiptext">Parcheggio</span></li>
+                    <li class="sb__service">
+                      <img  class="sb-service__image" src="/../img/lgicons/business/lgb-garden.png">
+                      <span class="tooltiptext">Parco privato</span></li>
+                  </ul>
+                  <img src="/../img/credit-card-logos.png"/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="vini" class="lg-section color-wine overlay--dark" style="background-image:url(http://langhe.net/wp-content/uploads/2016/07/Uve.jpg);">
+    <div class="inner padding--large has-folio--lg">
+      <div class="inner__row">
+        <div class="flex-column flex-column--2-3">
+          <div class="flex-column__content content-box">
+            <h2>I Vini</h2>
+            <h3>Vini del roero vini nel rispetto della natura</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="lg-section">
+    <div class="inner folio--lg">
+      <div class="inner__row">
+        <div class="flex-column flex-column--2-3 padding--medium color-main dropshadow">
+          <div class="flex-column__content content-box">
+            <p>Il <strong>Dogliani</strong> è un vino <strong>elegante</strong> ma non imegnativo, mentre il <strong>Superiore </strong>è più<strong> strutturato </strong>e<strong> complesso</strong> con un<strong> profumo speziato</strong> e una buona acidità.</p>
+
+            <p>Il <strong>Barolo</strong> è di Castiglione Falletto e si caratterizza per la <strong>prontezze di beva</strong></p>
+
+            <p>Capace di invecchiare a lungo, allo stesso tempo è pronto da bere dopo i 4 anni di invecchiamento obbligatorio.</p>
+
+            <p>Un altro grande rosso è un <strong>assemblaggio</strong> nato 2013 e dedicato a<strong> nonno Celso</strong>, il fondatore dell'azienda. Da qui il nome di questo vino composto da Barbera, Nebbiolo e Cabernet: <strong>Excelso</strong>.</p>
+
+            <p>Gli <strong>altri rossi</strong> vini sono <strong>Barbera</strong>, <strong>Nebbiolo</strong> e un <strong>rosato</strong>, cui si affiancano il <strong>bianco</strong> Roero <strong>Arneis</strong> e uno <strong>Spumante</strong> Metodo Classico.</p>
+          </div>
+        </div>
+        <div class="flex-column flex-column--1-3 padding--small not-equal--top color-wine washed">
+          <div class="flex-column__content content-box">
+            <h3 style="text-transform: uppercase;">Informazioni</h3>
+            <p style="font-family: 'Open Sans'"><strong>Ettari:</strong> 45<br/>
+              <strong>Locazione delle Vigne:</strong> Guarene, La Morra*</p>
+            <p style="font-family: 'Open Sans'"><strong>Bottiglie Prodotte:</strong> 10.000</p>
+            <p style="font-family: 'Open Sans'"><strong>Metodo Vitivinicolo:</strong> lotta integrata*</p>
+            <p style="font-family: 'Open Sans'"><strong>Biologico:</strong> Si<br/>
+              <strong>Biodinamico:</strong> No</p>
+            <p style="font-family: 'Open Sans'"><strong>Vini vini:</strong> Barolo DOCG, Dogliani Superiore DOCG, Dogliani DOCG, Langhe Nebbiolo DOC, Roero Arneis DOCG, Piemonte Barbera DOC, Langhe Rosso DOC, Rosato</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="visite" class="lg-section color-travel overlay--dark" style="background-image:url(http://langhe.net/wp-content/uploads/2016/07/vigne1.jpg); background-size: cover; background-position: center center;">
+    <div class="inner padding--large has-folio--lg">
+      <div class="inner__row">
+        <div class="flex-column flex-column--2-3">
+          <div class="flex-column__content content-box">
+            <h2>Visite & Degustazioni</h2>
+            <h3>Scopri le langhe attraverso i suoi cibi e i suoi vini</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="lg-section color-alternate--light">
+    <div class="inner folio--lg">
+      <div class="inner__row">
+        <div class="flex-column flex-column--2-3 padding--medium color-main dropshadow">
+          <div class="flex-column__content content-box">
+            <p><strong>All’esterno della cascina un ampio spazio coloniale</strong> e, un paesaggio di morbide colline interamente ricoperte di vigneti, danno alla famiglia Abbona la possibilità di poter<strong> ospitare le persone amanti del vino e della terra</strong> a visitare la cantina e i propri vigneti e, non per ultimo, a degustare i vini senza problemi di parcheggio per pullman e la sosta notturna dei camper.
+
+            <p>Si inizia con la <strong>degustazione dei vini guidata da Silvia e Sergio</strong>, i quali vi racconteranno la storia di ciascuna etichetta.</p>
+
+            <p>Ciascun bicchiere è accompagnato da <strong>assaggi di vini locali e di piatti tipici</strong> per un vero <strong>aperitivo in cantina</strong>.</p>
+
+            <p>Dopodiché ci si sposta nel<strong> locale di fermentazione e invecchiamento</strong>, per scoprire il processo di produzione fino all'imbottigliamento ed etichettatura.</p>
+            <p>La cantina offre due tipologie di visita in cantina:</p>
+            <ul>
+             	<li><strong>Gratuita</strong>: con assaggio di 3 vini e piccoli stuzzichini.</li>
+             	<li><strong>Completa</strong> a <strong>15 € a persona</strong>: con assaggio di tutti i vini accompagnati da salumi, formaggi, salse, marmellate e dolci e altri piatti tipici della cucina tradizionale piemontese.</li>
+            </ul>
+
+            <p>Su prenotazione è possibile ordinare <strong>vini senza glutine</strong> per una degustazione adatta ai celiaci.</p>
+
+            <p>Inoltre, è possibile concordare la partecipazione ai lavori in vigna (potatura invernale, potatura verde, vendemmia, etc.).</p>
+
+            <p>La cantina può ospitare <strong>grandi gruppi fino a 60 persone</strong>.</p>
+
+            <p>La visita dura circa 2 ore e le lingue disponibili sono <strong>italiano</strong>, <strong>inglese</strong>, <strong>francese</strong>, <strong>russo</strong> e <strong>cinese</strong>.</p>
+          </div>
+        </div>
+        <div class="flex-column flex-column--1-3 padding--none not-equal--top ">
+          <div class="inner__row">
+            <div class="flex-column flex-column--max padding--medium height--full-top color-travel washed">
+              <div class="flex-column__content content-box">
+                <div class="flex-column__content content-box">
+                  <h3 id="prenota" style="text-transform: uppercase;">PRENOTA</h3>
+                  <form class="form sb--form sb--visit-form">
+                    <div class="fields__container">
+                      <div class="field__container">
+                        <label>Nome</label>
+                        <input type="text">
+                      </div>
+                      <div class="field__container">
+                        <label>Cognome</label>
+                        <input type="text">
+                      </div>
+                      <div class="field__container">
+                        <label>Telefono</label>
+                        <input type="text">
+                      </div>
+                      <div class="field__container">
+                        <label>Email</label>
+                        <input type="text">
+                      </div>
+                      <div class="form__button-container">
+                        <input type="submit" value="Avanti" class="form__button">
+                      </div>
+                    </div>
+                  </form>
+                <!--  <ul>
+                    <li>Gratuita - 3 vini a scelta</li>
+                    <li>5€ - 6 vini</li>
+                    <li>Max 50 partecipanti</li>
+                  </ul> -->
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="inner__row">
+            <div class="flex-column flex-column--max padding--small color-alternate washed">
+              <div class="flex-column__content content-box">
+                <h3>Hai un camper?</h3>
+                <p>Nell’area verde che circonda la nostra azienda offriamo la possibilità di ospitare fino a 24-28 camper! Se ti interessa questa soluzione indicalo nelle note del modulo di prenotazione.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="contatti" class="lg-section color-travel washed">
+    <div class="inner fluid">
+      <div class="inner__row">
+        <div class="flex-column flex-column--max gutter--none special">
+          <div class="flex-column__content content-box">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2834.1078334555373!2d8.041512215533274!3d44.73781677909912!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4787f67256381cfd%3A0xc1dca538533d3db0!2sVia+Sotteri%2C+4%2C+12050+Sotteri+CN!5e0!3m2!1sen!2sit!4v1477471661457" width="100%" height="650" frameborder="0" style="border:0" allowfullscreen></iframe>
+          </div>
+        </div>
+        <div class="flex-column flex-column--1-3 padding--large diagonal dropshadow color-travel washed">
+          <div class="flex-column__content content-box">
+            <h2 style="margin-bottom:0;">CONTATTI</h2>
+            <h3 style="margin-top:0;">Azienda Agricola Langhero</h3>
+            <p><strong>Indirizzo</strong> Località Sotteri n. 4, 12050 Guarene (Cuneo), Italia</p>
+            <p><strong>tel:</strong> +393663180873<br/>
+            <strong>e-email:</strong> info@langhero.it<br/>
+            <strong>Sito-Web:</strong> www.langhero.it</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php
+     $path = $_SERVER['DOCUMENT_ROOT'];
+     $path .= "/includes/footer.html";
+     include_once($path);
+  ?>
